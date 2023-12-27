7ab84c76b34448e288766530d3148f0acd28e344
Finn Völkel <FiV0@users.noreply.github.com>
2023-12-27 09:46:53 +0100

    Initial commit

---
+++ build.clj
@@ -0,0 +1,30 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+(def lib 'clj.template/lib)
+(def version (format "0.1.%s" (b/git-count-revs nil)))
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+(def main 'main)
+
+(defn clean
+  "Cleans the target path."
+  [_]
+  (b/delete {:path "target"}))
+
+(defn jar [_]
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/compile-clj {:basis basis
+                  :src-dirs ["src"]
+                  :class-dir class-dir
+                  :bindings {#'clojure.core/*assert* false}})
+  (b/uber {:class-dir class-dir
+           :uber-file jar-file
+           :main main
+           :basis basis}))
+
+(comment
+  (clean nil)
+  (jar nil))
---
+++ src/main.clj
@@ -0,0 +1,5 @@
+(ns main
+  (:gen-class))
+
+(defn -main [& args]
+  (println "Hello from Clojure!!! " args))
