d0153156b721f1db68fc7b3679a38f4b9f85c3c4
Finn Völkel <finn.volkel@gmail.com>
2023-12-28 15:38:35 +0100

    layout

--- src/xt_fiddle/client.cljs
+++ src/xt_fiddle/client.cljs
@@ -13,41 +13,88 @@
   ;; 'my.app.thing :trace   ;; Some namespaces you might want detailed logging
   })
 
-(defn connect-button []
-  [:input {:type     :button
-           :value    "Connect"
-           :disabled @(rf/subscribe [:app/connected])
-           :on-click #(rf/dispatch [:app/connect])}])
+(rf/reg-event-db
+ :app/init
+ (fn [_ _]
+   {:type :xtql}))
 
-(defn input-field []
-  (let [state (r/atom {:text ""
-                       :error false})]
+(rf/reg-event-db
+ :dropdown-selection
+ (fn [db [_ selection]]
+   (assoc db :type selection)))
+
+(rf/reg-sub
+ :selection
+ (fn [db _]
+   (:type db)))
+
+
+(defn dropdown []
+  (let [open? (r/atom false)]
     (fn []
-      (cond-> [:div
-               [:textarea {:id "text-field"
-                           :rows 5
-                           :value (:text @state)
-                           :on-change #(swap! state assoc :text (.. % -target -value))}]
-               [:input {:type     :button
-                        :value    "Send"
-                        :disabled (not @(rf/subscribe [:app/connected]))
-                        :on-click #(try
-                                     (rf/dispatch [:app/send (cljs.reader/read-string (:text @state))])
-                                     (swap! state assoc :text "" :error false)
-                                     (catch js/Error _
-                                       (swap! state assoc :error true)))}]]
-        (:error @state) (conj [:div {:style {:color :red}} "Input not valid edn"])))))
+      [:div {:class "relative inline-block text-left"}
+       [:button {:type "button"
+                 :class "inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none"
+                 :id "dropdownDefault"
+                 :data-dropdown-toggle "dropdown"
+                 :on-click #(swap! open? not)}
+        (if (= :xtql @(rf/subscribe [:selection]))
+          "XTQL"
+          "SQL")
+        [:svg {:class"w-4 h-4 ml-2"
+               :xmlns "http://www.w3.org/2000/svg"
+               :fill "none"
+               :viewBox "0 0 24 24"
+               :stroke "currentColor"
+               :aria-hidden "true"}
+         [:path
+          {:stroke-linecap "round" :stroke-linejoin "round" :stroke-width "2" :d "M19 9l-7 7-7-7"}]]]
+
+       (when @open?
+         [:div {:class "z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700" :id "dropdown"}
+          [:ul {:class "py-1 text-sm text-gray-700 dark:text-gray-200" :aria-labelledby "dropdownDefault"}
+           [:li
+            [:a {:href "#" :class  "block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                 :on-click (fn [event]
+                             (.preventDefault event)
+                             (rf/dispatch [:dropdown-selection :xtql]))}
+             "XTQL"]]
+           [:li
+            [:a {:href "#" :class  "block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                 :on-click (fn [event]
+                             (.preventDefault event)
+                             (rf/dispatch [:dropdown-selection :sql]))}
+             "SQL"]]]])])))
 
 
 (defn app []
-  [:div
-   [:h2
-    "Hello from cljs"]])
+  [:div {:class "flex flex-col h-screen"}
+   [:header {:class "bg-gray-200 p-4 text-lg font-semibold shadow-md flex items-center justify-between" #_"bg-gray-200 p-4 text-lg font-semibold"}
+    [:div {:class "flex items-center space-x-4"}
+
+     [:h2 "XT fiddle"]
+     [:button  {:class "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"}
+      "Run"]
+
+     [dropdown]]]
+
+   [:div {:class "flex flex-1 overflow-hidden"}
+    [:aside {:class "w-64 bg-gray-100 p-4 overflow-auto"}
+     "Side"]
+    [:div {:class "flex flex-col flex-1 overflow-hidden"}
+     [:section {:class "flex flex-1 overflow-auto p-4"}
+      [:div {:class "flex-1 bg-white border mr-4 p-4"}
+       "Content 1"]
+      [:div {:class "flex-1 bg-white border p-4"}
+       "Content 2"]]
+     [:section {:class "flex-1 bg-white p-4 border-t border-gray-300" :style="flex-grow: 3;" }
+      "Bottom content"]]]])
+
 
 ;; start is called by init and after code reloading finishes
 (defn ^:dev/after-load start! []
   (js/console.log "start")
-  ;; (rf/dispatch-sync [:app/init])
+  (rf/dispatch-sync [:app/init])
   (reagent.dom/render [app] (js/document.getElementById "app")))
 
 (defn ^:export init []
