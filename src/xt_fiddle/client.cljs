ae7ed8484b606fbcd997e35255f5744b2b8b954e
Oliver Marshall <olivershawmarshall@gmail.com>
2024-03-22 09:45:49 +0000

    feat: remove the dropshadow

--- src/xt_fiddle/client.cljs
+++ src/xt_fiddle/client.cljs
@@ -118,7 +118,7 @@
         body))
 
 (defn header []
-  [:header {:class "bg-gray-200 py-2 shadow-md"}
+  [:header {:class "bg-gray-200 py-2"}
    [:div {:class "container mx-auto flex items-center space-x-4"}
     [title "XT fiddle"]
     [language-dropdown]
