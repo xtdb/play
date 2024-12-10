bf96065e36d342808e89a0e963637b1dac7563d0
Johanna Antonelli <joa@juxt.pro>
2024-12-10 14:18:30 +0000

    Extract db actions to make more testable

---
+++ src/clj/xt_play/xtdb.clj
@@ -0,0 +1,48 @@
+(ns xt-play.xtdb
+  (:require [clojure.tools.logging :as log]
+            [next.jdbc :as jdbc]
+            [next.jdbc.result-set :as jdbc-res]
+            [xt-play.config :as config]
+            [xtdb.api :as xt]
+            [xtdb.node :as xtn]))
+
+(defn with-xtdb [f]
+  (try
+    (with-open [node (xtn/start-node {})]
+      (f node))
+    (catch Exception e
+      (log/warn :submit-error {:e e})
+      (throw e))))
+
+(defn submit! [node txs opts]
+  (log/info :submit-tx txs opts)
+  (let [tx (xt/submit-tx node txs opts)
+        results (xt/q node '(from :xt/txs [{:xt/id $tx-id} xt/error])
+                      {:args {:tx-id (:tx-id tx)}})]
+    (log/info :submit-tx results)
+    ;; If any transaction fails, throw the error
+    (when-let [error (-> results first :xt/error)]
+      (throw error))))
+
+(defn query [node q]
+  (log/info :query q)
+  (xt/q node q (when (string? q) {:key-fn :snake-case-string})))
+
+(defn with-jdbc [f]
+  (with-open [conn (jdbc/get-connection config/db)]
+    (f conn)))
+
+(defn jdbc-execute! 
+  [conn statement]
+  (log/info :jdbc-execute! statement)
+  (jdbc/execute! conn statement {:builder-fn jdbc-res/as-arrays}))
+
+(comment
+  (with-open [node (xtn/start-node {})]
+    (doseq [st [#inst "2022" #inst "2021"]]
+      (let [tx (xt/submit-tx node [] {:system-time st})
+            results (xt/q node '(from :xt/txs [{:xt/id $tx-id} xt/error])
+                          {:basis {:at-tx tx}
+                           :args {:tx-id (:tx-id tx)}})]
+        (when-let [error (-> results first :xt/error)]
+          (throw (ex-info "Transaction error" {:error error})))))))
