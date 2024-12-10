82d03143886f808ffbc9520901e418eeb83520c1
Johanna Antonelli <joa@juxt.pro>
2024-12-10 14:18:30 +0000

    Extract db actions to make more testable

--- src/clj/xt_play/transactions.clj
+++ src/clj/xt_play/transactions.clj
@@ -3,12 +3,8 @@
             [clojure.data.json :as json]
             [clojure.instant :refer [read-instant-date]]
             [clojure.tools.logging :as log]
-            [next.jdbc :as jdbc]
-            [next.jdbc.result-set :as jdbc-res]
-            [xt-play.config :as config]
             [xt-play.util :as util]
-            [xtdb.api :as xt]
-            [xtdb.node :as xtn]))
+            [xt-play.xtdb :as xtdb]))
 
 (defn- encode-txs [tx-type txs]
   (case (keyword tx-type)
@@ -37,25 +33,6 @@
 (defn format-system-time [s]
   (when s (read-instant-date s)))
 
-(defn- run!-tx [node tx-type tx-batches query]
-  (let [tx-batches (->> tx-batches
-                        (map #(update % :system-time format-system-time))
-                        (map #(update % :txs (partial encode-txs tx-type))))]
-    (doseq [{:keys [system-time txs] :as batch} tx-batches]
-      (log/info tx-type "running batch: " batch)
-      (let [tx (xt/submit-tx node txs {:system-time system-time})
-            results (xt/q node '(from :xt/txs [{:xt/id $tx-id} xt/error])
-                          {:args {:tx-id (:tx-id tx)}})]
-        ;; If any transaction fails, throw the error
-        (log/info tx-type "batch complete:" tx ", results:" results)
-        (when-let [error (-> results first :xt/error)]
-          (throw error)))))
-  (log/info tx-type "running query:" query)
-  (let [res (xt/q node query (when (string? query)
-                               {:key-fn :snake-case-string}))]
-    (log/info tx-type "XTDB query response:" res)
-    res))
-
 (defn- PGobject->clj [v]
   (if (= org.postgresql.util.PGobject (type v))
     (json/read-str (.getValue v) :key-fn keyword)
@@ -70,16 +47,30 @@
      (mapv PGobject->clj row))
    result))
 
+(defn- run!-tx [node tx-type tx-batches query]
+  (let [tx-batches (->> tx-batches
+                        (map #(update % :system-time format-system-time))
+                        (map #(update % :txs (partial encode-txs tx-type))))]
+    (doseq [{:keys [system-time txs] :as batch} tx-batches]
+      (log/info tx-type "running batch: " batch)
+      (xtdb/submit! node txs {:system-time system-time})))
+  (log/info tx-type "running query:" query)
+  (let [res (xtdb/query node query)]
+    (def res res)
+    (log/info tx-type "XTDB query response:" res)
+    res))
+
 (defn- run!-with-jdbc-conn [tx-batches query]
-  (with-open [conn (jdbc/get-connection config/db)]
-    (doseq [tx (prepare-statements tx-batches)
-            statement tx]
-      (log/info "beta executing statement:" statement)
-      (jdbc/execute! conn statement))
-    (log/info "beta running query:" query)
-    (let [res (jdbc/execute! conn [query] {:builder-fn jdbc-res/as-arrays})]
-      (log/info "beta query resoponse" res)
-      (parse-result res))))
+  (xtdb/with-jdbc
+    (fn [conn]
+      (doseq [tx (prepare-statements tx-batches)
+              statement tx]
+        (log/info "beta executing statement:" statement)
+        (xtdb/jdbc-execute! conn statement))
+      (log/info "beta running query:" query)
+      (let [res (xtdb/jdbc-execute! conn [query])]
+        (log/info "beta query resoponse" res)
+        (parse-result res)))))
 
 (defn run!!
   "Given transaction batches, a query and the type of transaction to
@@ -87,35 +78,20 @@
   returning the last query response in column format."
   [{:keys [tx-batches query tx-type]}]
   (let [query (if (= "xtql" tx-type) (util/read-edn query) query)]
-    (try
-      (with-open [node (xtn/start-node {})]
+    (xtdb/with-xtdb
+      (fn [node]
         (if (= "sql-beta" tx-type)
           (run!-with-jdbc-conn tx-batches query)
-          (util/map-results->rows
-           (run!-tx node tx-type tx-batches query))))
-      (catch Exception e
-        (log/warn :submit-error {:e e})
-        (throw e)))))
+          (let [res (run!-tx node tx-type tx-batches query)]
+            (util/map-results->rows res)))))))
 
 (defn docs-run!!
   "Given transaction batches and a query from the docs, will return the query
   response in map format. Assumes tx type is sql."
   [{:keys [tx-batches query]}]
-  (try
-    (with-open [node (xtn/start-node {})]
+  (xtdb/with-xtdb
+    (fn [node]
       (run!-tx node "sql"
                (mapv #(update % :txs util/read-edn) tx-batches)
-               (util/read-edn query)))
-    (catch Exception e
-      (log/warn :submit-error {:e e})
-      (throw e))))
+               (util/read-edn query)))))
 
-(comment
-  (with-open [node (xtn/start-node {})]
-    (doseq [st [#inst "2022" #inst "2021"]]
-      (let [tx (xt/submit-tx node [] {:system-time st})
-            results (xt/q node '(from :xt/txs [{:xt/id $tx-id} xt/error])
-                          {:basis {:at-tx tx}
-                           :args {:tx-id (:tx-id tx)}})]
-        (when-let [error (-> results first :xt/error)]
-          (throw (ex-info "Transaction error" {:error error})))))))
