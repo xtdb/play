b949e8ed50b984e0893d2741643f4453d8c91f60
Johanna Antonelli <joa@juxt.pro>
2024-12-10 14:30:43 +0000

    Add clj tests

--- test/clj/xt_play/handler_test.clj
+++ test/clj/xt_play/handler_test.clj
@@ -1,196 +1,195 @@
 (ns xt-play.handler-test
   (:require [clojure.edn :as edn]
+            [clojure.string :as str]
             [clojure.test :as t]
             [clojure.data.json :as json]
-            [next.jdbc :as jdbc]
             [xt-play.handler :as h]
-            [xtdb.api :as xt]))
-
+            [xt-play.xtdb-stubs :refer [with-stubs db clean-db mock-resp reset-resp]]))
 ;; todo:
 ;; [ ] test unhappy paths
 ;; [ ] test wider range of scenarios / formats
-;; [ ] test to pipeline
+;; [x] test to pipeline
 ;; [ ] assert format from client
+;; [x] stub xtdb
+;; [x] integration tests with xtdb
+
 
 (defn- t-file [path]
   (edn/read-string (slurp (format "test-resources/%s.edn" path))))
 
 (t/deftest run-handler-test
-  (t/testing "xtql example returns expected results"
-    (t/is (= {:status 200, :body [[:foo :xt/id] ["bar" 1]]}
-             (h/run-handler (t-file "xtql-example-request")))))
+  (with-stubs
+    #(do
+       (t/testing "xtql example sends expected payload to xtdb"
+         (clean-db)
+         (h/run-handler (t-file "xtql-example-request"))
+         (let [[txs query] @db]
+           (t/is (= 2 (count @db)))
+           (t/is (= [[:put-docs :docs {:xt/id 1, :foo "bar"}]]
+                    txs))
+           (t/is (= '(from :docs [xt/id foo])
+                    query))))
 
-  (t/testing "sql example returns expected results"
-    (t/is (= {:status 200,
-              :body
-              [["_id" "col1" "col2"]
-               [2 "bar" " baz"]
-               [1 "foo" nil]]}
-             (h/run-handler (t-file "sql-example-request")))))
+       (t/testing "sql example sends expected payload to xtdb"
+         (clean-db)
+         (h/run-handler (t-file "sql-example-request"))
+         (let [[txs query] @db]
+           (t/is (= 2 (count @db)))
+           (t/is (= [[:sql "INSERT INTO docs (_id, col1) VALUES (1, 'foo')"]          
+                     [:sql "
+INSERT INTO docs RECORDS {_id: 2, col1: 'bar', col2:' baz'}"]]
+                  txs))
+           (t/is (= "SELECT * FROM docs"
+                  query))
+           (t/is (not (str/includes? (first txs) ";")))))
 
-  (t/testing "beta sql example returns expected results"
-    (t/is (= {:status 200,
-              :body
-              [[:_id :col1 :col2]
-               [2 "bar" " baz"]
-               [1 "foo" nil]]}
-             (h/run-handler (t-file "beta-sql-example-request"))))))
+       (t/testing "beta sql example sends expected payload to xtdb"
+         (clean-db)
+         (h/run-handler (t-file "beta-sql-example-request"))
+         (let [[txs query] @db]
+           (t/is (= 2 (count @db)))
+           (t/is (= ["INSERT INTO docs (_id, col1) VALUES (1, 'foo');
+INSERT INTO docs RECORDS {_id: 2, col1: 'bar', col2:' baz'};"]
+                  txs))
+           (t/is (= ["SELECT * FROM docs"]
+                  query))
+           (t/is (str/includes? (first txs) ";")))))))
 
 (t/deftest run-handler-multi-transactions-test
-  (t/testing "multiple transactions in xtql"
-    (t/is (= {:status 200, :body [[:foo :xt/id]
-                                  ["baz" 2]
-                                  ["bar" 1]]}
-             (h/run-handler
+  (with-stubs
+    #(do
+       (t/testing "multiple transactions in xtql"
+         (clean-db)
+         (h/run-handler (assoc-in
+                         (t-file "xtql-example-request")
+                         [:parameters :body :tx-batches]
+                         [{:txs "[:put-docs :docs {:xt/id 1 :foo \"bar\"}]",
+                           :system-time "2024-12-01T00:00:00.000Z"}
+                          {:txs "[:put-docs :docs {:xt/id 2 :foo \"baz\"}]",
+                           :system-time nil}]))
+         (let [[tx1 tx2 query] @db]
+           (t/is (= 3 (count @db)))
+           (t/is (= [[:put-docs :docs {:xt/id 1, :foo "bar"}]]
+                    tx1))
+           (t/is (= [[:put-docs :docs {:xt/id 2, :foo "baz"}]]
+                    tx2))
+           (t/is (= '(from :docs [xt/id foo])
+                    query))))
+
+       (t/testing "multiple transacions on sql"
+         (clean-db)
+         (h/run-handler
+          (-> (t-file "sql-example-request")
               (assoc-in
-               (t-file "xtql-example-request")
                [:parameters :body :tx-batches]
-               [{:txs "[:put-docs :docs {:xt/id 1 :foo \"bar\"}]",
+               [{:txs "INSERT INTO docs (_id, col1) VALUES (1, 'foo');",
                  :system-time "2024-12-01T00:00:00.000Z"}
-                {:txs "[:put-docs :docs {:xt/id 2 :foo \"baz\"}]",
-                 :system-time nil}])))))
-
-  (t/testing "multiple transacions on sql"
-    (t/is (= {:status 200,
-              :body
-              [["_id" "col1" "col2" "_valid_from"]
-               [2 "bar" " baz" #time/zoned-date-time "2024-12-02T00:00Z[UTC]"]
-               [1 "foo" nil #time/zoned-date-time "2024-12-01T00:00Z[UTC]"]]}
-             (h/run-handler
-              (-> (t-file "sql-example-request")
-                  (assoc-in
-                   [:parameters :body :tx-batches]
-                   [{:txs "INSERT INTO docs (_id, col1) VALUES (1, 'foo');",
-                     :system-time "2024-12-01T00:00:00.000Z"}
-                    {:txs "INSERT INTO docs RECORDS {_id: 2, col1: 'bar', col2:' baz'};",
-                     :system-time "2024-12-02T00:00:00.000Z"}])
-                  (assoc-in
-                   [:parameters :body :query]
-                   "SELECT *, _valid_from FROM docs"))))))
-
-  (t/testing "beta sql can run multiple txs"
-    (t/is (= {:status 200,
-              :body
-              [[:_id :col1 :col2 :_valid_from]
-               [2 "bar" " baz" #inst "2024-12-02T00:00:00.000000000-00:00"]
-               [1 "foo" nil #inst "2024-12-01T00:00:00.000000000-00:00"]]}
-             (h/run-handler
-              (-> (t-file "beta-sql-example-request")
-                  (assoc-in
-                   [:parameters :body :tx-batches]
-                   [{:txs "INSERT INTO docs (_id, col1) VALUES (1, 'foo');",
-                     :system-time "2024-12-01T00:00:00.000Z"}
-                    {:txs "INSERT INTO docs RECORDS {_id: 2, col1: 'bar', col2:' baz'};",
-                     :system-time "2024-12-02T00:00:00.000Z"}])
-                  (assoc-in
-                   [:parameters :body :query]
-                   "SELECT *, _valid_from FROM docs")))))))
-
-(t/deftest beta-sql-run-features
-  (t/testing "Column order is maintained"
-    (t/is (= {:status 200,
-              :body [[:_id :a :b :c :d :e :f :g :h :j]
-                     [1 2 3 4 5 6 7 8 9 10]]}
-             (h/run-handler
+                {:txs "INSERT INTO docs RECORDS {_id: 2, col1: 'bar', col2:' baz'};",
+                 :system-time "2024-12-02T00:00:00.000Z"}])
               (assoc-in
-               (t-file "beta-sql-example-request")
-               [:parameters :body :tx-batches]
-               [{:txs "INSERT INTO docs RECORDS {_id: 1, a: 2, b: 3, c: 4, d: 5, e: 6, f: 7, g: 8, h: 9, j: 10}"
-                 :system-time nil}])))))
-
-  (t/testing "execute payload is not mutated"
-    (let [txs (atom [])]
-      (with-redefs [jdbc/execute! (fn [_conn statement & args]
-                                    (swap! txs conj statement))]
-        (h/run-handler (t-file "beta-sql-example-request"))
-        (t/is
-         (= [[(str "INSERT INTO docs (_id, col1) VALUES (1, 'foo');\n"
-                   "INSERT INTO docs RECORDS {_id: 2, col1: 'bar', col2:' baz'};")]
-             ["SELECT * FROM docs"]]
-            @txs)))))
-
-  (t/testing "xt submit-tx sql payload is reformatted"
-    (let [txs (atom [])]
-      (def txs txs)
-      (with-redefs [xt/submit-tx (fn [_node tx & args]
-                                   (swap! txs conj tx))]
-        (h/run-handler (t-file "sql-example-request"))
-        (t/is
-         (= [[[:sql "INSERT INTO docs (_id, col1) VALUES (1, 'foo')"]
-              [:sql "\nINSERT INTO docs RECORDS {_id: 2, col1: 'bar', col2:' baz'}"]]]
-            @txs))))))
-
-(t/deftest sql-says-carol-is-red-test
-  (t/testing "XTDB docs example for sql https://docs.xtdb.com/quickstart/sql-overview.html"
-    (t/is (= {:status 200,
-              :body
-              [["name" "favorite_color" "_valid_from" "_system_from"]
-               ["carol"
-                "red"
-                #time/zoned-date-time "2023-09-01T00:00Z[UTC]"
-                #time/zoned-date-time "2024-01-08T00:00Z[UTC]"]]}
-             (h/run-handler (t-file "sql-multi-transaction")))))
-
-  (t/testing "Bob still likes fishing - don't determine columns based on the first row"
-    (t/is (= {:status 200,
-              :body
-              [["_id" "favorite_color" "name" "likes"]
-               [2 "red" "carol" nil]
-               [9 nil "bob" ["fishing" 3.14 {"nested" "data"}]]]}
-             (h/run-handler
-              (assoc-in
-               (t-file "sql-multi-transaction")
                [:parameters :body :query]
-               "SELECT * FROM people"))))))
+               "SELECT *, _valid_from FROM docs")))
+         (let [[tx1 tx2 query] @db]
+           (t/is (= 3 (count @db)))
+           (t/is (= [[:sql "INSERT INTO docs (_id, col1) VALUES (1, 'foo')"]]
+                    tx1))
+           (t/is (= [[:sql "INSERT INTO docs RECORDS {_id: 2, col1: 'bar', col2:' baz'}"]]
+                    tx2))
+           (t/is (= "SELECT *, _valid_from FROM docs"
+                    query)))
+         )
 
-(t/deftest beta-sql-says-carol-is-red-test
-  (t/testing "XTDB docs example for sql https://docs.xtdb.com/quickstart/sql-overview.html"
-    (t/is (= {:status 200,
-              :body
-              [[:name :favorite_color :_valid_from :_system_from]
-               ["carol"
-                "red"
-                #inst "2023-08-31T23:00:00.000000000-00:00"
-                #inst "2024-01-08T00:00:00.000000000-00:00"]]}
-             (h/run-handler (assoc-in
-                             (t-file "sql-multi-transaction")
-                             [:parameters :body :tx-type]
-                             "sql-beta")))))
+       (t/testing "multiple transacions on beta sql"
+         (clean-db)
+         (h/run-handler
+          (-> (t-file "beta-sql-example-request")
+              (assoc-in
+               [:parameters :body :tx-batches]
+               [{:txs "INSERT INTO docs (_id, col1) VALUES (1, 'foo');",
+                 :system-time "2024-12-01T00:00:00.000Z"}
+                {:txs "INSERT INTO docs RECORDS {_id: 2, col1: 'bar', col2:' baz'};",
+                 :system-time "2024-12-02T00:00:00.000Z"}])
+              (assoc-in
+               [:parameters :body :query]
+               "SELECT *, _valid_from FROM docs")))
 
-  (t/testing "Bob still likes fishing - don't determine columns based on the first row"
-    (t/is (= {:status 200,
-             :body
-             [[:_id :favorite_color :info :likes :name]
-              [2 "red" nil nil "carol"]
-              [9 nil nil ["fishing" 3.14 {:nested "data"}] "bob"]]}
-             (h/run-handler
-              (->  (t-file "sql-multi-transaction")
-                   (assoc-in [:parameters :body :query] "SELECT * FROM people")
-                   (assoc-in [:parameters :body :tx-type] "sql-beta")))))))
+         (t/is (= 7 (count @db)))
+         (t/is (= [["BEGIN AT SYSTEM_TIME TIMESTAMP '2024-12-01T00:00:00.000Z'"]
+                   ["INSERT INTO docs (_id, col1) VALUES (1, 'foo');"]
+                   ["COMMIT"]
+                   ["BEGIN AT SYSTEM_TIME TIMESTAMP '2024-12-02T00:00:00.000Z'"]
+                   ["INSERT INTO docs RECORDS {_id: 2, col1: 'bar', col2:' baz'};"]
+                   ["COMMIT"]
+                   ["SELECT *, _valid_from FROM docs"]]
+                  @db))))))
+
+(t/deftest do-not-drop-columns
+  (with-stubs
+    #(do
+       (t/testing "Bob still likes fishing - don't determine columns based on the first row"
+         (mock-resp [{"_id" 2, "favorite_color" "red", "name" "carol"}
+                     {"_id" 9, "likes" ["fishing" 3.14 {"nested" "data"}], "name" "bob"}])
+         (t/is (= {:status 200,
+                   :body
+                   [["_id" "favorite_color" "name" "likes"]
+                    [2 "red" "carol" nil]
+                    [9 nil "bob" ["fishing" 3.14 {"nested" "data"}]]]}
+                  (h/run-handler
+                   (assoc-in
+                    (t-file "sql-multi-transaction")
+                    [:parameters :body :query]
+                    "SELECT * FROM people")))))
+       (reset-resp))))
 
 (def docs-json
- "{\"tx-batches\":[{\"txs\":\"[[:sql \\\"INSERT INTO product (_id, name, price) VALUES\\\\n(1, 'An Electric Bicycle', 400)\\\"]]\",\"system-time\":\"2024-01-01\"},{\"txs\":\"[[:sql \\\"UPDATE product SET price = 405 WHERE _id = 1\\\"]]\",\"system-time\":\"2024-01-05\"},{\"txs\":\"[[:sql \\\"UPDATE product SET price = 350 WHERE _id = 1\\\"]]\",\"system-time\":\"2024-01-10\"}],\"query\":\"\\\"SELECT *, _valid_from\\\\nFROM product\\\\nFOR VALID_TIME ALL -- i.e. \\\\\\\"show me all versions\\\\\\\"\\\\nFOR SYSTEM_TIME AS OF DATE '2024-01-31' -- \\\\\\\"...as observed at month end\\\\\\\"\\\"\"}"
-)
+  "{\"tx-batches\":[{\"txs\":\"[[:sql \\\"INSERT INTO product (_id, name, price) VALUES\\\\n(1, 'An Electric Bicycle', 400)\\\"]]\",\"system-time\":\"2024-01-01\"},{\"txs\":\"[[:sql \\\"UPDATE product SET price = 405 WHERE _id = 1\\\"]]\",\"system-time\":\"2024-01-05\"},{\"txs\":\"[[:sql \\\"UPDATE product SET price = 350 WHERE _id = 1\\\"]]\",\"system-time\":\"2024-01-10\"}],\"query\":\"\\\"SELECT *, _valid_from\\\\nFROM product\\\\nFOR VALID_TIME ALL -- i.e. \\\\\\\"show me all versions\\\\\\\"\\\\nFOR SYSTEM_TIME AS OF DATE '2024-01-31' -- \\\\\\\"...as observed at month end\\\\\\\"\\\"\"}"
+  )
 
 (t/deftest docs-run
-  (let [response (h/docs-run-handler {:parameters {:body (json/read-str docs-json :key-fn keyword)}})]
-    (t/testing "docs run returns map results"
-      (t/is
-       (every? map? (:body response))))
+  (with-stubs
+    #(do
+       (clean-db)
+       (mock-resp [{"_id" 1,
+                    "name" "An Electric Bicycle",
+                    "price" 350,
+                    "_valid_from" #time/zoned-date-time "2024-01-10T00:00Z[UTC]"}
+                   {"_id" 1,
+                    "name" "An Electric Bicycle",
+                    "price" 405,
+                    "_valid_from" #time/zoned-date-time "2024-01-05T00:00Z[UTC]"}
+                   {"_id" 1,
+                    "name" "An Electric Bicycle",
+                    "price" 400,
+                    "_valid_from" #time/zoned-date-time "2024-01-01T00:00Z[UTC]"}])
+       
+       (let [response (h/docs-run-handler {:parameters {:body (json/read-str docs-json :key-fn keyword)}})
+             txs (drop-last @db)
+             query (last @db)]
 
-    (t/testing "can handle \" strings from docs"
-      (t/is (= {:status 200,
-                :body
-                [{"_id" 1,
-                  "name" "An Electric Bicycle",
-                  "price" 350,
-                  "_valid_from" #time/zoned-date-time "2024-01-10T00:00Z[UTC]"}
-                 {"_id" 1,
-                  "name" "An Electric Bicycle",
-                  "price" 405,
-                  "_valid_from" #time/zoned-date-time "2024-01-05T00:00Z[UTC]"}
-                 {"_id" 1,
-                  "name" "An Electric Bicycle",
-                  "price" 400,
-                  "_valid_from" #time/zoned-date-time "2024-01-01T00:00Z[UTC]"}]}
-               response)))))
+         (t/is (= [[[:sql "INSERT INTO product (_id, name, price) VALUES\n(1, 'An Electric Bicycle', 400)"]]
+                   [[:sql "UPDATE product SET price = 405 WHERE _id = 1"]]
+                   [[:sql "UPDATE product SET price = 350 WHERE _id = 1"]]]
+                  txs))
+
+         (t/is (= "SELECT *, _valid_from\nFROM product\nFOR VALID_TIME ALL -- i.e. \"show me all versions\"\nFOR SYSTEM_TIME AS OF DATE '2024-01-31' -- \"...as observed at month end\""
+                  query))
+         
+         (t/testing "docs run returns map results"
+           (t/is (every? map? (:body response))))
+
+         (t/testing "can handle \" strings from docs"
+           (t/is (= {:status 200,
+                     :body
+                     [{"_id" 1,
+                       "name" "An Electric Bicycle",
+                       "price" 350,
+                       "_valid_from" #time/zoned-date-time "2024-01-10T00:00Z[UTC]"}
+                      {"_id" 1,
+                       "name" "An Electric Bicycle",
+                       "price" 405,
+                       "_valid_from" #time/zoned-date-time "2024-01-05T00:00Z[UTC]"}
+                      {"_id" 1,
+                       "name" "An Electric Bicycle",
+                       "price" 400,
+                       "_valid_from" #time/zoned-date-time "2024-01-01T00:00Z[UTC]"}]}
+                    response)))))))
---
+++ test/clj/xt_play/xtdb_stubs.clj
@@ -0,0 +1,40 @@
+(ns xt-play.xtdb-stubs
+  (:require [clojure.tools.logging :as log]
+            [xt-play.xtdb :as xtdb]))
+
+(def db (atom []))
+(defn- execute! [statement] (swap! db conj statement))
+(defn clean-db [] (reset! db []))
+
+(def resp (atom nil))
+(defn mock-resp [response] (reset! resp response))
+(defn reset-resp [] (mock-resp nil))
+
+(defn with-xtdb [f] (f nil))
+
+(defn submit! [_node txs opts]
+  (log/info :stub-submit-tx txs opts)
+  (execute! txs))
+
+(defn query [_node q]
+  (log/info :stub-query q)
+  (execute! q)
+  (or
+   @resp
+   [{"_id" 2, "col1" "bar", "col2" " baz"} {"_id" 1, "col1" "foo"}]))
+
+(defn with-jdbc [f] (f nil))
+
+(defn jdbc-execute! [_conn statement]
+  (log/info :stub-jdbc-execute! statement)
+  (execute! statement)
+  (or @resp
+      [[:_id :col1 :col2] [2 "bar" " baz"] [1 "foo" nil]]))
+
+(defn with-stubs [f]
+  (with-redefs [xtdb/with-xtdb with-xtdb
+                xtdb/with-jdbc with-jdbc
+                xtdb/submit! submit!
+                xtdb/query query
+                xtdb/jdbc-execute! jdbc-execute!]
+    (f)))
