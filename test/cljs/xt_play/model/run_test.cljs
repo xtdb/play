0cdc5d0374d95f04981e0833411104cc133dc583
Jeremy Taylor <jdt@juxt.pro>
2025-01-16 15:16:09 +0000

    upgrade to beta5

--- test/cljs/xt_play/model/run_test.cljs
+++ test/cljs/xt_play/model/run_test.cljs
@@ -5,7 +5,7 @@
    [xt-play.model.tx-batch :as batch]))
 
 (def app-db
-  {:version "2.0.0-beta3",
+  {:version "2.0.0-beta5",
    :type :sql,
    :query "SELECT *, _valid_from FROM docs",
    ::batch/list [::batch/tx5],
