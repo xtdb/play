30783158e568c6776527b10fcca4489414750b74
Oliver Marshall <olivershawmarshall@gmail.com>
2025-06-11 16:59:18 +0100

    feat: bump to 2.0.0 🥳🎉

--- test/cljs/xt_play/model/run_test.cljs
+++ test/cljs/xt_play/model/run_test.cljs
@@ -5,7 +5,7 @@
    [xt-play.model.tx-batch :as batch]))
 
 (def app-db
-  {:version "2.0.0-beta9",
+  {:version "2.0.0",
    :type :sql,
    :query "SELECT *, _valid_from FROM docs",
    ::batch/list [::batch/tx5],
